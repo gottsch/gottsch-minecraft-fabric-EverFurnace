5bdcef3ad592bd6879d271f8464d1c3d4bf4f071
gottsch <17928819+gottsch@users.noreply.github.com>
2024-12-09 10:39:25 -0500

    update CHANGELOG.md

---
+++ src/main/java/mod/gottsch/fabric/everfurnace/core/EverFurnace.java
@@ -0,0 +1,33 @@
+/*
+ * This file is part of  EverFurnace.
+ * Copyright (c) 2024 Mark Gottschling (gottsch)
+ *
+ * EverFurnace is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EverFurnace is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with EverFurnace.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+package mod.gottsch.fabric.everfurnace.core;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EverFurnace implements ModInitializer {
+	public static final String MOD_ID = "everfurnace";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
